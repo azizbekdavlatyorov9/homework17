9c8854ebaac17445f4cca280d8f1c3815636cbbf
Azizbek Davlatyorov <azizbekdavlatyorov9@gmail.com>
2026-06-29 14:06:56 +0500

    add file

--- drawSQL-pgsql-export-2026-06-29.sql
+++ drawSQL-pgsql-export-2026-06-29.sql
@@ -165,13 +165,13 @@ CREATE TABLE "events"(
 ALTER TABLE
     "events" ADD PRIMARY KEY("id");
 CREATE TABLE "event_rsvps"(
-    "id" BIGINT NOT NULL,
+    "event_id" BIGINT NOT NULL,
     "user_id" BIGINT NOT NULL,
     "answer" VARCHAR(255) NOT NULL,
     "answered_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
 ALTER TABLE
-    "event_rsvps" ADD PRIMARY KEY("id");
+    "event_rsvps" ADD PRIMARY KEY("event_id");
 CREATE TABLE "proposals"(
     "id" BIGINT NOT NULL,
     "talk_id" BIGINT NOT NULL,
@@ -213,7 +213,97 @@ CREATE TABLE "talks"(
 );
 ALTER TABLE
     "talks" ADD PRIMARY KEY("id");
+CREATE TABLE "proposal_ratings"(
+    "proposal_id" BIGINT NOT NULL,
+    "grade" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "created_by" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "proposal_ratings" ADD PRIMARY KEY("proposal_id");
+CREATE TABLE "comments"(
+    "event_id" BIGINT NOT NULL,
+    "proposal_id" BIGINT NULL,
+    "id" BIGINT NOT NULL,
+    "kind" VARCHAR(255) NOT NULL,
+    "answers" VARCHAR(255) NULL,
+    "text" VARCHAR(255) NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "created_by" CHAR(255) NOT NULL
+);
+ALTER TABLE
+    "comments" ADD PRIMARY KEY("event_id");
+ALTER TABLE
+    "sponsor_packs" ADD CONSTRAINT "sponsor_packs_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id");
+ALTER TABLE
+    "sponsors" ADD CONSTRAINT "sponsors_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id");
+ALTER TABLE
+    "proposal_ratings" ADD CONSTRAINT "proposal_ratings_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "events" ADD CONSTRAINT "events_cfp_id_foreign" FOREIGN KEY("cfp_id") REFERENCES "cfps"("id");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_speakers_foreign" FOREIGN KEY("speakers") REFERENCES "Users"("id");
+ALTER TABLE
+    "events" ADD CONSTRAINT "events_orga_notes_updated_by_foreign" FOREIGN KEY("orga_notes_updated_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "comments" ADD CONSTRAINT "comments_proposal_id_foreign" FOREIGN KEY("proposal_id") REFERENCES "proposals"("id");
+ALTER TABLE
+    "events" ADD CONSTRAINT "events_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id");
+ALTER TABLE
+    "contacts" ADD CONSTRAINT "contacts_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "partners"("logo");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "proposal_ratings" ADD CONSTRAINT "proposal_ratings_proposal_id_foreign" FOREIGN KEY("proposal_id") REFERENCES "proposals"("id");
+ALTER TABLE
+    "sponsors" ADD CONSTRAINT "sponsors_partner_id_foreign" FOREIGN KEY("partner_id") REFERENCES "partners"("id");
+ALTER TABLE
+    "talks" ADD CONSTRAINT "talks_speakers_foreign" FOREIGN KEY("speakers") REFERENCES "Users"("id");
+ALTER TABLE
+    "event_rsvps" ADD CONSTRAINT "event_rsvps_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "Users"("id");
+ALTER TABLE
+    "contacts" ADD CONSTRAINT "contacts_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "partners"("social_twitter");
+ALTER TABLE
+    "comments" ADD CONSTRAINT "comments_event_id_foreign" FOREIGN KEY("event_id") REFERENCES "events"("id");
+ALTER TABLE
+    "talks" ADD CONSTRAINT "talks_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_event_id_foreign" FOREIGN KEY("event_id") REFERENCES "events"("id");
+ALTER TABLE
+    "event_rsvps" ADD CONSTRAINT "event_rsvps_event_id_foreign" FOREIGN KEY("event_id") REFERENCES "events"("id");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_talk_id_foreign" FOREIGN KEY("talk_id") REFERENCES "talks"("id");
+ALTER TABLE
+    "comments" ADD CONSTRAINT "comments_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "contacts" ADD CONSTRAINT "contacts_partner_id_foreign" FOREIGN KEY("partner_id") REFERENCES "partners"("id");
+ALTER TABLE
+    "partners" ADD CONSTRAINT "partners_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "cfps" ADD CONSTRAINT "cfps_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id");
+ALTER TABLE
+    "partners" ADD CONSTRAINT "partners_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "Users"("id");
 ALTER TABLE
     "sponsors" ADD CONSTRAINT "sponsors_contact_id_foreign" FOREIGN KEY("contact_id") REFERENCES "contacts"("id");
 ALTER TABLE
+    "venues" ADD CONSTRAINT "venues_partner_id_foreign" FOREIGN KEY("partner_id") REFERENCES "partners"("id");
+ALTER TABLE
+    "cfps" ADD CONSTRAINT "cfps_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "talks" ADD CONSTRAINT "talks_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "proposals" ADD CONSTRAINT "proposals_cfp_id_foreign" FOREIGN KEY("cfp_id") REFERENCES "cfps"("id");
+ALTER TABLE
+    "cfps" ADD CONSTRAINT "cfps_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "events" ADD CONSTRAINT "events_updated_by_foreign" FOREIGN KEY("updated_by") REFERENCES "Users"("id");
+ALTER TABLE
+    "logins" ADD CONSTRAINT "logins_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "Users"("id");
+ALTER TABLE
+    "events" ADD CONSTRAINT "events_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
+ALTER TABLE
     "venues" ADD CONSTRAINT "venues_contact_id_foreign" FOREIGN KEY("contact_id") REFERENCES "contacts"("id");
+ALTER TABLE
+    "partners" ADD CONSTRAINT "partners_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "Users"("id");
